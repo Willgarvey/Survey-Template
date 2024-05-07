7b7c523cc448f9a9d64183ccbec2b5172b23ac6b
Will Garvey <willmgarvey@gmail.com>
2024-05-06 21:14:20 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    // List all of the components that will change
+    let example = document.getElementById('example');
+
+
+    // TODO: Create the folders with the images for the survey
+
+    // Decide on the questions fro the survey
+
+    // Figure out the logic for moving between the instances of the question
+        // Maybe use an internal counter to track which question we are on
+        // Like a for loop with page error handling to make sure that an
+        // answer is selected before moving on to the next page
+    
+    // Figure out how to make an embed for the result st opost to social media
+
+    startButton.addEventListener("click", function () {   
+
+    });
+}
